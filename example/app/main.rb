03408e54eb567e4561998c3fa6b874513200497d
Kasumi Hanazuki <kasumi@rollingapple.net>
2023-12-28 07:26:24 +0000

    example: Use Oj gem to test if native extension bundling works

--- example/app/main.rb
+++ example/app/main.rb
@@ -1,11 +1,9 @@
-require 'nokogiri'
+p Dir.pwd
+
+require 'bundler/setup'
+require 'oj'
 
 def handler(*)
-  {
-    libxml: Nokogiri::LIBXML_VERSION,
-  }
-end
-
-def handler2(*)
-  "hi!"
+  # This handler ensures the gem with a native extension is bundled properly
+  Oj.dump({greeting: 'hi!'})
 end
